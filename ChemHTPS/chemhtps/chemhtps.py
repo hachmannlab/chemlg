edf41abf153d005d042909b295a682ca811b507c
Bill Evangelista <wevangel@buffalo.edu>
2015-06-29 21:51:10 -0400

    Switched from optionparser to argparser

--- ChemHTPS/chemhtps/chemhtps.py
+++ ChemHTPS/chemhtps/chemhtps.py
@@ -42,8 +42,7 @@ DESCRIPTION = "ChemHTPS is a virtual high-throughput screening program suite for
 import sys
 import os
 import time
-# TODO: this should at some point replaced with argparser
-from optparse import OptionParser
+import argparse
 
 from misc import (banner,
                   format_invoked_opts,
@@ -63,13 +62,13 @@ from job_generator import (generate_jobs,
 
 ###################################################################################################
 
-def main(opts,commline_list):
+def main(args,commline_list):
     """(main):
         Driver of ChemHTPS.
     """
     time_start = time.time()
-    logfile = open(opts.logfile,'a',0)
-    error_file = open(opts.error_file,'a',0)
+    logfile = open(args.logfile,'a',0)
+    error_file = open(args.error_file,'a',0)
 
     banner_list = banner(PROGRAM_NAME, PROGRAM_VERSION, REVISION_DATE, AUTHORS, CONTRIBUTORS, DESCRIPTION)
     for line in banner_list:
@@ -78,7 +77,7 @@ def main(opts,commline_list):
 
 # TODO: implement read function for local config file which populates additional opts
 
-    fopts_list = format_invoked_opts(opts,commline_list)
+    fopts_list = format_invoked_opts(args,commline_list)
     for line in fopts_list:
         print line
         logfile.write(line + '\n')
@@ -88,11 +87,11 @@ def main(opts,commline_list):
     logfile.write(tmp_str + '\n')
 
 
-    if opts.setup_project:
-# TODO: write test that opts.project_name exists
-        setup_project(opts.project_name)
+    if args.setup_project:
+# TODO: write test that args.project_name exists
+        setup_project(args.project_name)
 
-    if opts.generatelib:
+    if args.generatelib:
         generate_structurelib()
         populate_db("moleculegraph")
         generate_geometries()
@@ -100,10 +99,10 @@ def main(opts,commline_list):
 
 # TODO: we may want to put a db-based bookkeeping step in here                
 
-    if opts.generatejobs:
+    if args.generatejobs:
         generate_jobs()
 
-    if opts.prioritizepool:
+    if args.prioritizepool:
         prioritize_pool()
 
 # TODO: add parser function
@@ -119,74 +118,74 @@ def main(opts,commline_list):
     error_file.close()
         
     # check whether error_file contains content
-    chk_rmfile(opts.error_file)
+    chk_rmfile(args.error_file)
     
     return 0    #successful termination of program
     
 ##################################################################################################
 
 if __name__=="__main__":
-    usage_str = "usage: %prog [options] arg"
-    version_str = "%prog " + PROGRAM_VERSION
-# TODO: replace with argparser; make sure that this doesn't break things in the code that rely on the opts info (e.g., the banner)
-    parser = OptionParser(usage=usage_str, version=version_str)    
+    usage_str = "usage: %(prog)s [options] arg"
+    version_str = "%(prog)s " + PROGRAM_VERSION
+    parser = argparse.ArgumentParser(usage=usage_str)
 
 # TODO: we should implement a way of automatically recognizing that chemhtps is launched from within a project folder (e.g., by hte presence of a config file), so that all this doesn't have to be specified every time
 # TODO: see additional comments about use of a chemhtps.config file above
-    parser.add_option('--project_name', 
-                      dest='project_name', 
-                      type='string', 
-                      help='name of the current project [default: %default]')
+    parser.add_argument('--version',
+                        action='version',
+                        version=version_str)
 
-    parser.add_option('--setup_project', 
-                      dest='setup_project', 
-                      action='store_true', 
-                      default=False,
-                      help='sets up the infrastructure for a new project [default: %default]')
+    parser.add_argument('--project_name',
+                        dest='project_name',
+                        default='new_project',
+                        help='name of the current project [default: %(default)s]')
 
-    parser.add_option('--generatelib', 
-                      dest='generatelib', 
-                      action='store_true', 
-                      default=False,
-                      help='generates a new screening library [default: %default]')
+    parser.add_argument('--setup_project',
+                        dest='setup_project',
+                        action='store_true',
+                        default=False,
+                        help='sets up the infrastructure for a new project [default: %(default)s]')
 
-    parser.add_option('--generatejobs', 
-                      dest='generatejobs', 
-                      action='store_true', 
-                      default=False,
-                      help='generates computational chemistry jobs [default: %default]')
+    parser.add_argument('--generatelib',
+                        dest='generatelib',
+                        action='store_true',
+                        default=False,
+                        help='generates a new screening library [default: %(default)s]')
 
-    parser.add_option('--prioritizepool', 
-                      dest='prioritizepool', 
-                      action='store_true', 
-                      default=False,
-                      help='prioritizes the jobs pool [default: %default]')
+    parser.add_argument('--generatejobs',
+                        dest='generatejobs',
+                        action='store_true',
+                        default=False,
+                        help='generates computational chemistry jobs [default: %(default)s]')
+
+    parser.add_argument('--prioritizepool',
+                        dest='prioritizepool',
+                        action='store_true',
+                        default=False,
+                        help='prioritizes the jobs pool [default: %(default)s]')
 
 
     # specify log files 
-    parser.add_option('--logfile', 
-                      dest='logfile', 
-                      type='string', 
-                      default='ChemHTPS.log',  
-                      help='specifies the name of the log-file [default: %default]')
+    parser.add_argument('--logfile',
+                        dest='logfile',
+                        default='ChemHTPS.log',
+                        help='specifies the name of the log-file [default: %(default)s]')
 
-    parser.add_option('--errorfile', 
-                      dest='error_file', 
-                      type='string', 
-                      default='ChemHTPS.err',  
-                      help='specifies the name of the error-file [default: %default]')
+    parser.add_argument('--errorfile',
+                        dest='error_file',
+                        default='ChemHTPS.err',
+                        help='specifies the name of the error-file [default: %(default)s]')
 
-    parser.add_option('--print_level', 
-                      dest='print_level', 
-                      type='int', 
-                      default=2, 
-                      help='specifies the print level for on screen and the logfile [default: %default]')
+    parser.add_argument('--print_level',
+                        dest='print_level',
+                        default=2,
+                        help='specifies the print level for on screen and the logfile [default: %(default)s]')
 
 
-    opts, args = parser.parse_args(sys.argv[1:])
+    args = parser.parse_args(sys.argv[1:])
     if len(sys.argv) < 2:
         sys.exit("You tried to run ChemHTPS without options.")
-    main(opts,sys.argv)   #numbering of sys.argv is only meaningful if it is launched as main
+    main(args, sys.argv)   #numbering of sys.argv is only meaningful if it is launched as main
     
 else:
     sys.exit("Sorry, must run as driver...")
