5346dca75fcafadcad90f2267f92550149a63703
mojtaba <mojtabah@buffalo.edu>
2018-11-14 20:08:57 -0800

    initial commit

---
+++ chemlg/setup.py
@@ -0,0 +1,77 @@
+import setuptools
+from os import path
+import chemml
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='chemml',
+        version=chemml.__version__,
+        author='Mojtaba Haghighatlari, Johannes Hachmann',
+        author_email='mojtabah@buffalo.edu, hachmann@buffalo.edu',
+        # url='https://github.com/hachmannlab/chemml',
+        project_urls={
+            'Source': 'https://github.com/hachmannlab/chemml',
+            'url': 'https://hachmannlab.github.io/chemml/'
+        },
+        description=
+        'A Machine Learning and Informatics Program Suite for the Chemical and Materials Sciences',
+        long_description=long_description,
+        keywords=[
+            'Machine Learning', 'Data Mining', 'Quantum Chemistry',
+            'Materials Science', 'Drug Discovery'
+        ],
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+
+        install_requires=[
+            'future', 'six', 'numpy', 'pandas',
+            'tensorflow', 'keras', 'h5py',
+            'scikit-learn', 'babel>=2.3.4',
+            'matplotlib>=1.5.1', 'deap>=1.2.2',
+            'lxml', 'wget'
+        ],
+        extras_require={
+            'docs': [
+                'sphinx',
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Natural Language :: English',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+        ],
+        zip_safe=False,
+    )
+
+    # install_requires = ['numpy>=1.13', 'pandas>=0.20.3', 'tensorflow==1.1.0', 'keras==2.1.5',
+    #                     'scikit-learn==0.19.1', 'babel>=2.3.4', 'matplotlib>=1.5.1', 'deap>=1.2.2',
+    #                     'lxml','nose','ipywidgets>=7.1','widgetsnbextension>=3.1','graphviz'],
+
+    # include_package_data = True,
+    # package_data={
+    #                 '': ['*.xyz', '*.csv', '*.vasp', '*.txt'],
+    #                 # 'cheml': ['datasets/data/*', 'tests/data/*', 'tests/configfiles/*'],
+    #             },
---
+++ setup.py
@@ -0,0 +1,66 @@
+import setuptools
+from os import path
+import chemlg
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='chemlg',
+        version=chemlg.__version__,
+        author='Mohammad Atif Faiz Afzal, Johannes Hachmann',
+        author_email='m27@buffalo.edu, hachmann@buffalo.edu',
+        # url='https://github.com/hachmannlab/chemml',
+        project_urls={
+            'Source': 'https://github.com/hachmannlab/chemlg',
+            'url': 'https://hachmannlab.github.io/chemlg/'
+        },
+        description=
+        'ChemLG is a smart and massive parallel molecular library generator for  chemical and materials sciences.',
+        long_description=long_description,
+        scripts=['lib/chemlg'],
+        keywords=[
+            'Library Generator', 'Molecular Library',
+            'Materials Science', 'Drug Discovery'
+        ],
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+
+        install_requires=[
+            'future', 'six', 'numpy', 'pandas',
+            'scipy',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx',
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Natural Language :: English',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
+        zip_safe=False,
+    )
