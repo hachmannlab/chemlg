7aac86e9cceb3177b4eaba74885353da8854bed7
mojtaba <mojtabah@buffalo.edu>
2018-11-23 11:16:56 -0800

    example of test files for other developers

---
+++ tests/libgen/test_molecule.py
@@ -0,0 +1,23 @@
+"""
+This is a test file for the molecule function in the libgen module.
+"""
+import pytest
+
+from chemlg.libgen import molecule
+
+@pytest.fixture()
+def smiles_code():
+    return ('CcC', 'F1-F2')
+
+
+def test_general(smiles_code):
+    mol = molecule(*smiles_code)
+    assert isinstance(mol, dict)
+    assert mol['can_smiles'] == 'C[CH]C\t\n'
+    assert mol['code'] == 'F1-F2'
+    assert mol['smiles'] == 'CcC'
+    assert mol['reverse_smiles'] == 'C[CH]C'
+
+def test_exceptions(smiles_code):
+    with pytest.raises(OSError):
+        mol = molecule('asdf', 'F1-F2')
