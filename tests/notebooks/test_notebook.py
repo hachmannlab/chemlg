a6185c19d9de895a6cb5361efb1f703b17b93d02
mojtaba <mojtabah@buffalo.edu>
2018-11-24 11:28:49 -0800

    initial commit

---
+++ tests/notebooks/test_notebook.py
@@ -0,0 +1,4 @@
+"""
+Think about test files for the notebooks module. we also need to polish that folder. 
+Maybe we should test non-notebook parts of the code here to increase the code coverage. 
+"""
