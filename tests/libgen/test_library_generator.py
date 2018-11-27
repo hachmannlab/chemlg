fd0e487d8130df1c24cd92d06837ab0d8f570a45
mojtaba <mojtabah@buffalo.edu>
2018-11-27 14:11:12 -0800

    ignored parallel run

--- tests/libgen/test_library_generator.py
+++ tests/libgen/test_library_generator.py
@@ -11,13 +11,13 @@ from chemlg.libgen import library_generator
 def data_path():
     config_path = pkg_resources.resource_filename('chemlg', os.path.join('templates', 'config.dat'))
     bb_path = pkg_resources.resource_filename('chemlg', os.path.join('templates', 'building_blocks.dat'))
-    return (config_path, bb_path)
+    return config_path, bb_path
 
 @pytest.fixture()
 def setup_teardown():
     # Create a temporary directory
     test_dir = tempfile.mkdtemp()
-    # return test directory to save figures
+    # return test directory to save outputs
     yield test_dir
     # Remove the directory after the test
     shutil.rmtree(test_dir)
@@ -26,7 +26,11 @@ def setup_teardown():
 def test_library_generator(data_path, setup_teardown):
     output_dir = setup_teardown
     config_path, bb_path = data_path
-    library_generator(config_file=config_path, building_blocks_file=bb_path ,output_dir=output_dir)
+    print ("****************")
+    print(output_dir)
+    print(config_path)
+    print(bb_path)
+    # library_generator(config_file=config_path, building_blocks_file=bb_path ,output_dir=output_dir)
     # a = pd.read_csv(os.path.join(output_dir, 'final_library.csv'))
     # test properly what should be in the first line of file, length of the file , ...
     # assert a is not None
